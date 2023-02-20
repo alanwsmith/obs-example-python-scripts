6972c349f2559e40bd6f5d3c0f464b46716f5614
Alan Smith <commit@alanwsmith.com>
2023-02-20 16:37:35 -0500

    Basic start and stop buttons working

---
+++ src/start_stop_button.py
@@ -0,0 +1,47 @@
+import obspython as obs
+
+################################################
+# This one works. The check for the
+# stop button happens every tick which
+# means that if the tick is long it could
+# take a while for the stop to register.
+# That could be deal with but it's not
+# something I need at the moment. 
+################################################
+
+
+global running
+running = False
+
+def do_tick():
+    obs.blog(300, "tick")
+    if running == False:
+        obs.remove_current_callback()
+
+def start(props, prop):
+    obs.blog(300, "Starting Test")
+    global running 
+    running = True
+    obs.timer_add(do_tick, 400)
+
+def stop(props, prop):
+    obs.blog(300, "Stopping Test")
+    global running 
+    running = False
+
+def script_description():
+    return "A basic set of start/stop buttons for controlling a script"
+
+def script_properties():
+    props = obs.obs_properties_create()
+    obs.obs_properties_add_button(
+        props, "start_button", "Start", start
+    )
+
+    obs.obs_properties_add_button(
+        props, "stop_button", "Stop", stop
+    )
+
+    return props
+
+
